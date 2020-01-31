88efd41437dbe9ad1a45a9ffdbf4713095c5cc5f
baguskto <baguskto@gmail.com>
2020-01-31 14:59:00 +0700

    fragment detail category

--- app/src/main/java/com/example/made/Fragment/CategoryFragment.kt
+++ app/src/main/java/com/example/made/Fragment/CategoryFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.FragmentManager
 
 import com.example.made.R
 
@@ -32,7 +33,28 @@ class CategoryFragment : Fragment(), View.OnClickListener{
 
     override fun onClick(v: View) {
     if (v.id == R.id.btn_detail_category){
+        val mDetailCategoryFragment = DetailCategoryFragment()
 
+        val mBundle = Bundle()
+        mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle")
+        val description = "Kategori ini akan berisi produk-produk lifestyle"
+
+        mDetailCategoryFragment.arguments = mBundle
+        mDetailCategoryFragment.description = description
+
+        /*
+        Method addToBackStack akan menambahkan fragment ke backstack
+        Behaviour dari back button akan cek fragment dari backstack,
+        jika ada fragment di dalam backstack maka fragment yang akan di close / remove
+        jika sudah tidak ada fragment di dalam backstack maka activity yang akan di close / finish
+         */
+
+        val mFragmentManager = fragmentManager as FragmentManager
+        mFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_container,mDetailCategoryFragment, DetailCategoryFragment::class.java.simpleName)
+            .addToBackStack(null)
+            .commit()
     }
     }
 
