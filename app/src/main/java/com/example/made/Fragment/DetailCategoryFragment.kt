1f7555dc20b668ce2a5076bb045ed57a68f0a55e
baguskto <baguskto@gmail.com>
2020-02-03 16:24:54 +0700

    dealog pada fragment

--- app/src/main/java/com/example/made/Fragment/DetailCategoryFragment.kt
+++ app/src/main/java/com/example/made/Fragment/DetailCategoryFragment.kt
@@ -1,6 +1,7 @@
 package com.example.made.Fragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 import com.example.made.R
 import kotlinx.android.synthetic.main.fragment_detail_category.*
@@ -16,21 +18,23 @@ class DetailCategoryFragment : Fragment(), View.OnClickListener {
 
     var description : String? = null
 
-    companion object{
-        var EXTRA_NAME = "extra_name"
-        var EXTRA_DESCRIPTION = "extra_description"
-    }
 
     lateinit var tvCategoryName: TextView
     lateinit var  tvCategoryDescription: TextView
     lateinit var btnProfile : Button
     lateinit var  btnShowDialog : Button
 
+
+    companion object{
+        var EXTRA_NAME = "extra_name"
+        var EXTRA_DESCRIPTION = "extra_description"
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail_category, container, false)
     }
 
@@ -61,6 +65,12 @@ class DetailCategoryFragment : Fragment(), View.OnClickListener {
         }
     }
 
+         override fun onSaveInstanceState(outState: Bundle) {
+             super.onSaveInstanceState(outState)
+
+             outState.putString(EXTRA_DESCRIPTION, description)
+         }
+
     override fun onClick(v: View) {
         when (v.id){
             R.id.btn_profile -> {
@@ -68,6 +78,10 @@ class DetailCategoryFragment : Fragment(), View.OnClickListener {
             }
 
             R.id.btn_show_dialog -> {
+                val mOptionFialFragment = OptionDialogFragment()
+
+                val mFragmentManager = childFragmentManager
+                mOptionFialFragment.show(mFragmentManager,OptionDialogFragment::class.java.simpleName)
 
             }
 
@@ -75,5 +89,10 @@ class DetailCategoryFragment : Fragment(), View.OnClickListener {
 
     }
 
+    internal var optionDialogListener:  OptionDialogFragment.OnOptionDialogListener = object : OptionDialogFragment.OnOptionDialogListener{
+        override fun onOptionChosen(text: String?) {
+            Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
